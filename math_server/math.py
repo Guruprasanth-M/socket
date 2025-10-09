c392d90ef822c3f3db4437bb7ea4448386cf5f62
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2025-10-09 16:45:17 +0000

    math_server.py

--- math_server/math.py
+++ math_server/math.py
@@ -1,7 +1,24 @@
 from subprocess import Popen, PIPE, STDOUT
-p = Popen(['bc','-q','-i'],stdout=PIPE,stdin=PIPE,stderr=STDOUT)
+from threading import Thread
+class ProcessOutputThread(Thread):
+    def __init__(self,proc):
+        Thread.__init__(self)
+        self.proc = proc
+    
+    def run(self):
+        while not self.proc.stdout.closed:
+                print(self.proc.stdout.readline().rstrip().decode())
 
-#print(p.stdout.readline().rstrip())
-result = p.communicate('1+2\n'.encode())
-print("type = "+str(type(result)))
-print(result[0].decode())
+p = Popen(['bc','-q','-i'],stdout=PIPE,stdin=PIPE,stderr=STDOUT,shell=True)
+Output = ProcessOutputThread(p)
+Output.start()
+
+while not p.stdout.closed:
+    query = input(">>")
+    if query == "quit":
+        p.communicate(query.encode(),timeout=1)
+        if p.poll() is not None:
+            break
+    query = query + "\n"
+    p.stdin.write(query.encode())
+    p.stdin.flush()
