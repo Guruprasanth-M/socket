130e54cabcdc2f62a8c1b48acb0141e5e6fd79c9
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2025-11-29 17:59:13 +0000

    to be continue 10 video finshed

--- multithreaded_chat_server/multithreaded_chat.py
+++ multithreaded_chat_server/multithreaded_chat.py
@@ -35,13 +35,17 @@ class ChatBotThread(Thread):
     def run(self):
         while True:
             sleep(0.025) #25ms
+            # print(f"BOT message queue : {len(self.messages)}")
+            # print(f"BOT thread length : {len(self.threads)}")
             if len(self.messages) > 0:
                 for thread in self.threads:
                     for data in self.messages:
                         user = data[0]
                         msg  = data[1]
-                        if thread.getusername() != user[0]:
-                            thread.sendMessage(msg)                            
+                        if thread.getusername() != user:
+                            print(f"[BOT_send_message] {user}:{message}")
+                            thread.sendMessage(msg)
+                            self.message.remove(data)                            
 
                             
                             
@@ -56,8 +60,9 @@ class ChatserverOutgoingThread(Thread):
         self.messages = []
         self.can_kill = False
 
-    def sendMessage(self,message):
-        fMessage = f"{self.username} : {message}"
+    def sendMessage(self, user, message):
+        # use the function argument `user`
+        fMessage = f"{user} : {message}"
         try:
             conn = self.incoming_thread.getConnection()
             conn.sendall(fMessage.encode())
@@ -65,8 +70,11 @@ class ChatserverOutgoingThread(Thread):
             bot.removeChatThread(self.incoming_thread)
             self.killThread()
 
-    def queueMessage(self, message):
-        self.messages.append(message)
+
+    def queueMessage(self, user, message):
+        data = (user, message)
+        self.messages.append(data)
+        print(f"BOT queue : {user}:{message}")
 
     def killThread(self, should_inform = False):
         #inform other that the client has disconnected.
@@ -77,14 +85,21 @@ class ChatserverOutgoingThread(Thread):
             sleep(0.1) #100 milli sec
             if self.can_kill:
                 break #if this loop breaks,the thread has to end.
+
             if len(self.messages) > 0:
-                for message in self.messages:
+                # IMPORTANT: take a copy and clear, or the same message
+                # will be sent again and again forever
+                current_messages = self.messages[:]
+                self.messages.clear()
+
+                for message in current_messages:
                     try:
-                        self.sendMessage(message);
+                        print(f"[send_message] {message[0]}:{message[1]}")
+                        self.sendMessage(message[0], message[1])
+                        self.message.remove(message)
                     except:
                         #inform other that the client has disconnected.
                         break
-
 
 
 
@@ -96,7 +111,7 @@ class ChatServerIncomingThread(Thread):
         self.username = ""
         self.user_ip = addr[0]
         self.user_port = addr[1]
-        self.incoming_thread = None
+        self.outgoing_thread = None
         self.can_kill = False
 
     def setusername(self,username):
@@ -111,22 +126,41 @@ class ChatServerIncomingThread(Thread):
     def isClosed(self):
         return self.conn._closed
 
-    def initSendMessageThread(self,message):
-        self.incoming_thread = ChatserverOutgoingThread(self)
+    def initSendMessageThread(self):
+        self.outgoing_thread = ChatserverOutgoingThread(self)
+        self.outgoing_thread.start()
 
     def sendMessage(self,message):
-        self.incoming_thread.queueMessage(message)
+        self.outgoing_thread.queueMessage(self.getUsername(),message)
+
+    def broadcostMessage(self, message):
+        self.outgoing_thread.queueMessage("Server BOT",message)
 
     def killThread(self):
         bot.removeChatThread(self)
         self.can_kill = True
 
     def run(self):
+        self.initSendMessageThread()
+        sleep(0.2)
+        self.broadcostMessage("Welcome to the Group Chat server...\n")
+        self.broadcostMessage(f"You're Connected from {self.user_ip}:{self.user_port}\n")
+        self.broadcostMessage("Please enter your name to continue: ")
+        name = self.conn.recv(1024)
+        if not name:
+            self.outgoing_thread.killThread()
+            self.killThread()
+            return
+        else:
+            self.setusername(name.decode()) 
+        self.broadcostMessage("You can Chat with out Group...\n")   
+
         while not self.conn._closed:
             data = self.conn.recv(1024)
             if not data: #means client has disconnected.
                 # inform other that the client has disconnected.
-                self.incoming_thread.killThread()
+                self.outgoing_thread.killThread()
+                self.killThread()
                 break
             if data.decode().strip() == "kill":
                 self.killThread()
@@ -141,7 +175,7 @@ HOST = ''
 PORT = 7777
 
 bot = ChatBotThread()
-
+bot.start()
 binding = (HOST,PORT)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
@@ -150,13 +184,18 @@ s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
                 # to reuse the same address 
 s.bind(binding) # bind the socket to the address
 s.listen() # listen for connections
+try:
+    while not s._closed:
+        conn, addr = s.accept() # accept connection from client
+        #Move the accepted connection to a new thread
+        t = ChatServerIncomingThread(conn,addr)
+        t.start()
+        bot.addChatThread(t)
+except KeyboardInterrupt:
+    print("\n[+] Server shutting down...")
+    s.close()
+    exit(0)
 
-while not s._closed:
-    conn, addr = s.accept() # accept connection from client
-    #Move the accepted connection to a new thread
-    t = ChatServerIncomingThread(conn,addr)
-    t.start()
-    bot.addChatThread(t)
 
 if not s._closed:
     s.close()
